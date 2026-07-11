f3776d55cc7267dc406307481fa7fbabd63ad7be
Wirada Insuan <67160411@go.buu.ac.th>
2026-07-11 15:33:18 +0700

    Add: 2.2

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Student API พร้อมใช้งาน" });
 });
 
+let students = [
+  { id: 1, name: "สมชาย ใจดี", major: "วิทยาการคอมพิวเตอร์" },
+  { id: 2, name: "สมหญิง รักเรียน", major: "เทคโนโลยีสารสนเทศ" },
+];
+let nextId = 3;
+
 app.listen(PORT, () => {
   console.log(`Server กำลังทำงานที่ http://localhost:${PORT}`);
 });
